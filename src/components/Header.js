018e2bcb375c2042d6aed4e7cc089d11a9200a25
Aneesha Panigrahi <aneez1100@gmail.com>
2021-07-28 22:19:20 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import "./Header.css";
+// import "./Header.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { Dropdown, Button, ButtonGroup, Navbar, Nav, NavDropdown, FormControl, Form } from "react-bootstrap";
-import { Scrollbars } from "react-custom-scrollbars";
 
 const userId = window.location.href.split("/")[5];
 const Header = () => {
@@ -101,145 +100,7 @@ const Header = () => {
             </Nav>
   </Navbar.Collapse>
 </Navbar>
-      {/* <nav id="header-nav" className="navbar navbar-expand-lg navbar-light ">
-        <a href="/homePage" className="navbar-brand">
-          Mirror
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div
-          className="collapse navbar-collapse text-center"
-          id="navbarSupportedContent"
-        >
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="/homePage">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/customizationPage">
-                Customize
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/myAccount">
-                Shop
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-                href="/"
-              >
-                Account
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown" style={{ zIndex: '1500' }}>
-                <a className="dropdown-item" href="/myAccount">
-                  My account
-                </a>
-                <a className="dropdown-item" href="/myOrders">
-                  returns and orders
-                </a>
-
-                <a className="dropdown-item" href="/settings">
-                  Settings
-                </a>
-                <a className="dropdown-item" onClick={() => logout()} href="/">
-                  Logout
-                </a>
-              </div>
-            </li>
-          </ul>
-          <form>
-            <input
-              className="form-control"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-              name="search"
-              onChange={(e) => handleTextChange(e)}
-              value={searchQuery}
-              autoComplete="off"
-            />
-
-            <Link to={`/search/all/${userId}/${searchQuery}`}>
-              <button type="submit">
-                <i className="fa fa-search" />
-              </button>
-            </Link>
-          </form>
-
-          <div className="icons container-fluid">
-            <a href="" className="fa fa-bell dropdown" id="navbarDropdown" type="button" data-toggle="dropdown" aria-expanded="true" />
-            <div size="2" className="dropdown-menu" aria-labelledby="navbarDropdown" id="navbarDropdowndiv" style={{ width: "300px", maxHeight: "600px", zIndex: '2000' }}>
-              <Dropdown.Header>Notifications</Dropdown.Header>
-              <Dropdown.Divider />
-              <div>
-                <Dropdown.Item eventKey="1">
-                  <div>
-                    <img src="https://cdn3.iconfinder.com/data/icons/users-yellow/60/50_-Blank_Profile-_user_people_group_team-512.png" alt="" style={{ height: '50px', width: "auto", borderRadius: "50%", margin: "0px 10px 10px 0px" }} />
-                Designer1
-                <p style={{ color: "gray" }}>I will charge Rs.100 for your post </p>
-                    <ButtonGroup>
-                      <Button variant="light" style={{ color: "black", backgroundColor: "#ffd662ff", borderColor: "#ffd662ff", borderRadius: "4px", marginRight: "5px" }}>Accept</Button>
-                      <Button variant="secondary" style={{ borderRadius: "4px" }}>Reject</Button>
-                    </ButtonGroup>
-                  </div>
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item eventKey="2">
-                  <img src="https://www.asiancancerinstitute.com/public/images/blank-user.png" alt="" style={{ height: '50px', width: "auto", borderRadius: "50%", margin: "0px 10px 10px 0px" }} />
-                Designer2
-                <p style={{ color: "gray" }}>I will charge Rs.100 for your post </p>
-                  <ButtonGroup>
-                    <Button variant="light" style={{ color: "black", backgroundColor: "#ffd662ff", borderColor: "#ffd662ff", borderRadius: "4px", marginRight: "5px" }}>Accept</Button>
-                    <Button variant="secondary" style={{ borderRadius: "4px" }}>Reject</Button>
-                  </ButtonGroup>
-                </Dropdown.Item>
-                <Dropdown.Divider />
-              </div>
-            </div>
-            <Link to={`/${userId}/wishList`}>
-              <i
-                href="/"
-                className="fa fa-heart"
-                style={{ position: "relative" }}
-              >
-                <span className="badge badge-pill badge-light">
-                  {wishCount}
-                </span>
-              </i>
-            </Link>
-
-            <Link to={`/cart/all/${userId}`}>
-              <i
-                className="fa fa-shopping-cart"
-                style={{ position: "relative" }}
-              >
-                <span className="badge badge-pill badge-light">
-                  {cartCount}
-                </span>
-              </i>
-            </Link>
-          </div>
-        </div>
-      </nav> */}
+     
     </header>
   );
 };
